607f2d961d0a529cdddcc4db7f3b6b09372f29ca
Rodrigo Gonzalez <rodrigo.gonzalez@hey.com>
2025-12-21 13:40:44 +0400

    Test retail store layer

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,9 +1,14 @@
 import { DeckGL } from '@deck.gl/react'
-import { BASEMAP } from '@deck.gl/carto'
+import { BASEMAP, VectorTileLayer, vectorTableSource } from '@deck.gl/carto'
 import Maplibre from 'react-map-gl/maplibre'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import type { ReactNode } from 'react'
 import type { MapViewState } from '@deck.gl/core'
+
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL
+const accessToken = import.meta.env.VITE_API_ACCESS_TOKEN
+const connectionName = 'carto_dw'
+const cartoConfig = { apiBaseUrl, accessToken, connectionName }
 
 const INITIAL_VIEW_STATE: MapViewState = {
   latitude: 39.8097343,
@@ -13,9 +18,25 @@ const INITIAL_VIEW_STATE: MapViewState = {
   pitch: 30,
 }
 
+const retailStoresSource = vectorTableSource({
+  ...cartoConfig,
+  tableName: 'carto-demo-data.demo_tables.retail_stores',
+})
+
+const RetailStoresLayer = new VectorTileLayer({
+  id: 'retail-stores',
+  data: retailStoresSource,
+  pointRadiusMinPixels: 3,
+  getFillColor: [200, 0, 80],
+})
+
 export default function Map({ children }: { children?: ReactNode }) {
   return (
-    <DeckGL initialViewState={INITIAL_VIEW_STATE} controller>
+    <DeckGL
+      initialViewState={INITIAL_VIEW_STATE}
+      controller
+      layers={[RetailStoresLayer]}
+    >
       <Maplibre mapStyle={BASEMAP.VOYAGER}>{children}</Maplibre>
     </DeckGL>
   )
